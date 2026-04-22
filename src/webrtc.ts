7ac042b94b1e78422b8f0d5e313a7e8867e03b11
dimiy7805-ship-it <dimiy7805@gmail.com>
2026-04-22 16:15:37 +0200

    feat(webrtc): Add more STUN/TURN servers
    Includes additional STUN servers from Google and more TURN server options for improved connectivity reliability.

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -32,15 +32,20 @@ function stopRingtone() {
 
 const rtcConfig = {
     iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: [
+            'stun:stun.l.google.com:19302',
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302',
+            'stun:stun3.l.google.com:19302',
+            'stun:stun4.l.google.com:19302',
+            'stun:openrelay.metered.ca:80'
+        ]},
         {
-            urls: 'turn:openrelay.metered.ca:80',
-            username: 'openrelayproject',
-            credential: 'openrelayproject'
-        },
-        {
-            urls: 'turn:openrelay.metered.ca:443',
+            urls: [
+                'turn:openrelay.metered.ca:80',
+                'turn:openrelay.metered.ca:443',
+                'turn:openrelay.metered.ca:443?transport=tcp'
+            ],
             username: 'openrelayproject',
             credential: 'openrelayproject'
         }
